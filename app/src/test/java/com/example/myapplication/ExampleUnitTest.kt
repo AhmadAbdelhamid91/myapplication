4cd0531b7553ceb349c016bd498b4f45c9545ce7
just a bad developer <ahmad.abdelhamid35@gmail.com>
2023-06-09 06:43:52 +0300

    add linting

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
