76c97e402fd6f4ec84f8c21b1b5afaa5695b546d
just a bad developer <ahmad.abdelhamid35@gmail.com>
2023-06-10 01:27:42 +0300

    workflow 3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
+        setContent { // aaa
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
