dcebd46ce8820aa51d4d162b43e328ab15af52ef
just a bad developer <ahmad.abdelhamid35@gmail.com>
2023-06-10 01:04:58 +0300

    fix workflow

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { // test two work flows 2
+        setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
