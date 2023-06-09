fcc4e0b3dbff246c502e1a25a113d3f63e062ec3
just a bad developer <ahmad.abdelhamid35@gmail.com>
2023-06-09 22:47:49 +0300

    try pre-commit hooks

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,8 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        setContent {  //  aaaaaaaaa
+        setContent { // aaaaaaaaa
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
