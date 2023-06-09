d77180cfebe010a3076c970982a78bb0ecf7bb70
Ahmad Abdelhamid91 <amado.ahmad50@gmail.com>
2023-06-10 00:49:31 +0300

    separate workfllows (#4)
    * separate workfllows
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    ---------
    
    Co-authored-by: just a bad developer <ahmad.abdelhamid35@gmail.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { // aaaaaaaaa
+        setContent { // test two work flows
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
