8793df2706656be7b69367c9d2a3e9381c79bd0f
just a bad developer <ahmad.abdelhamid35@gmail.com>
2023-06-09 20:50:22 +0300

    try hooks 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,7 +15,7 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {//aaaaaaaaa
+        setContent { // aaaaaaaaa
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
